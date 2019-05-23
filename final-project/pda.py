e4b9edb5c9d892e23819f67e5723a221635493f3
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2019-05-23 20:22:56 +0700

    feat: Add PDA Community Detector

---
+++ final-project/pda.py
@@ -0,0 +1,23 @@
+import os
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+sc = SparkContext.getOrCreate()
+sc.addPyFile(os.path.join(os.environ['SPARK_HOME'], 'jars', 'graphframes-0.7.0-spark2.4-s_2.11.jar'))
+spark = SparkSession.builder.master('local').getOrCreate()
+
+with open('data/user_list.txt') as infile:
+    user_list = infile.read().split('\n')[:-1]
+
+user_list = list(enumerate(map(int, user_list)))
+vertices = spark.createDataFrame(user_list, ['id', 'name'])
+
+def process_batch(batch_filename, batch_num):
+    import graphframes
+    print('processing', batch_filename)
+    edges = spark.read.csv(batch_filename, header=True)
+    graph = graphframes.GraphFrame(vertices, edges)
+    communities = graph.labelPropagation(maxIter=5)
+    comm_filename = 'data/models/comm_{}.csv'.format(batch_num)
+    communities.write.csv(comm_filename, header=True, mode='overwrite')
